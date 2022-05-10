cca80b9935201c089d35316b5199dce17ae713c0
Seraphina09 <103226787+seraphina09@users.noreply.github.com>
2022-05-10 10:43:36 +0900

    Create InflearnMvc2ApplicationTests.java

---
+++ Inflearn-mvc2-master/Inflearn-mvc2-master/src/test/java/me/catsbi/inflearnmvc2/InflearnMvc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.catsbi.inflearnmvc2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnMvc2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
