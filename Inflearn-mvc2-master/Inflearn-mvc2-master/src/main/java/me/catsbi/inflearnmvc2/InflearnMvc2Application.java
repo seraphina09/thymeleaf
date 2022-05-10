17885c3e25d613bcf5284dd09b2656af05f77c99
Seraphina09 <103226787+seraphina09@users.noreply.github.com>
2022-05-10 10:45:47 +0900

    Create InflearnMvc2Application.java

---
+++ Inflearn-mvc2-master/Inflearn-mvc2-master/src/main/java/me/catsbi/inflearnmvc2/InflearnMvc2Application.java
@@ -0,0 +1,13 @@
+package me.catsbi.inflearnmvc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnMvc2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnMvc2Application.class, args);
+    }
+
+}
